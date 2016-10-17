4dd7c054d2e4832c27d42422ff1b2a20722153e0
Xavier Caron <xcaron@gmail.com>
2016-10-17 13:36:19 +0200

    for consistency's sake

--- tail_compare.c
+++ tail_compare.c
@@ -6,11 +6,11 @@
 // identifiers consisting of only digits are compared numerically and identifiers with letters or hyphens
 // are compared lexically in ASCII sort order. Numeric identifiers always have lower precedence than
 // non-numeric identifiers. A larger set of pre-release fields has a higher precedence than a smaller set,
-// if all of the preceding identifiers are TAIL_COMPARE_EQual. Example:
+// if all of the preceding identifiers are TAIL_CMP_EQual. Example:
 // 1.0.0-alpha < 1.0.0-alpha.1 < 1.0.0-alpha.beta < 1.0.0-beta < 1.0.0-beta.2 < 1.0.0-beta.11 < 1.0.0-rc.1 < 1.0.0.
 
 int tail_cmp ( char *lhs, char *rhs ) {
-  if ( !strcmp ( lhs, rhs ) ) return TAIL_COMPARE_EQ;
+  if ( !strcmp ( lhs, rhs ) ) return TAIL_CMP_EQ;
 
   char *dot = ".";
   char *l_last, *r_last;
@@ -18,8 +18,8 @@ int tail_cmp ( char *lhs, char *rhs ) {
   char *l_token = strtok_r ( lhs, dot, &l_last );
   char *r_token = strtok_r ( rhs, dot, &r_last );
 
-  if (  l_token && !r_token ) return TAIL_COMPARE_LT;
-  if ( !l_token &&  r_token ) return TAIL_COMPARE_GT;
+  if (  l_token && !r_token ) return TAIL_CMP_LT;
+  if ( !l_token &&  r_token ) return TAIL_CMP_GT;
 
   while ( l_token || r_token ) {
     if ( l_token && r_token ) {
@@ -30,41 +30,41 @@ int tail_cmp ( char *lhs, char *rhs ) {
         int l_int = atoi ( l_token );
         int r_int = atoi ( r_token );
 
-        if ( l_int < r_int ) return TAIL_COMPARE_LT;
-        if ( l_int > r_int ) return TAIL_COMPARE_GT;
+        if ( l_int < r_int ) return TAIL_CMP_LT;
+        if ( l_int > r_int ) return TAIL_CMP_GT;
       }
       else if ( l_numeric ) {
-        return TAIL_COMPARE_LT;
+        return TAIL_CMP_LT;
       }
       else if ( r_numeric ) {
-        return TAIL_COMPARE_GT;
+        return TAIL_CMP_GT;
       }
       else {
         int cmp = strcmp ( l_token, r_token );
 
-        if ( cmp ) return cmp > 0 ? TAIL_COMPARE_GT : TAIL_COMPARE_LT;
+        if ( cmp ) return cmp > 0 ? TAIL_CMP_GT : TAIL_CMP_LT;
       }
     }
     else if ( l_token ) {
-      return TAIL_COMPARE_GT;
+      return TAIL_CMP_GT;
     }
     else if ( r_token ) {
-      return TAIL_COMPARE_LT;
+      return TAIL_CMP_LT;
     }
 
     l_token = strtok_r ( NULL, dot, &l_last );
     r_token = strtok_r ( NULL, dot, &r_last );
   }
 
-  return TAIL_COMPARE_KO;
+  return TAIL_CMP_KO;
 }
 
 int tail_lt ( char *lhs, char *rhs ) {
-  return tail_cmp ( lhs, rhs ) == TAIL_COMPARE_LT;
+  return tail_cmp ( lhs, rhs ) == TAIL_CMP_LT;
 }
 int tail_eq ( char *lhs, char *rhs ) {
-  return tail_cmp ( lhs, rhs ) == TAIL_COMPARE_EQ;
+  return tail_cmp ( lhs, rhs ) == TAIL_CMP_EQ;
 }
 int tail_gt ( char *lhs, char *rhs ) {
-  return tail_cmp ( lhs, rhs ) == TAIL_COMPARE_GT;
+  return tail_cmp ( lhs, rhs ) == TAIL_CMP_GT;
 }
--- tail_compare.h
+++ tail_compare.h
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TAIL_COMPARE_LT -1
-#define TAIL_COMPARE_EQ  0
-#define TAIL_COMPARE_GT +1
-#define TAIL_COMPARE_KO  9
+#define TAIL_CMP_LT -1
+#define TAIL_CMP_EQ  0
+#define TAIL_CMP_GT +1
+#define TAIL_CMP_KO  9
 
 int tail_cmp ( char* lhs, char* rhs );
 
