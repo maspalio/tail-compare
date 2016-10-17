80306032fdd1be4c924afb74a19fe5bc70505ae2
Xavier Caron <xcaron@gmail.com>
2016-10-17 10:22:43 +0200

    an embryo of implementation

--- tail_compare.c
+++ tail_compare.c
@@ -1,19 +1,65 @@
 #include "tail_compare.h"
 
-bool tail_lt ( char *lhs, char *rhs ) {
-  printf ( "lhs: %s\n", lhs );
-  // printf ( "rhs: %s\n", rhs );
+// Precedence for two pre-release versions with the same major, minor, and patch version MUST be determined
+// by comparing each dot separated identifier from left to right until a difference is found as follows:
+// identifiers consisting of only digits are compared numerically and identifiers with letters or hyphens
+// are compared lexically in ASCII sort order. Numeric identifiers always have lower precedence than
+// non-numeric identifiers. A larger set of pre-release fields has a higher precedence than a smaller set,
+// if all of the preceding identifiers are equal.
+// Example:
+// 1.0.0-alpha < 1.0.0-alpha.1 < 1.0.0-alpha.beta < 1.0.0-beta < 1.0.0-beta.2 < 1.0.0-beta.11 < 1.0.0-rc.1 < 1.0.0.
 
+bool tail_lt ( char *lhs, char *rhs ) {
   char xlhs[256], xrhs[256];
   strcpy ( xlhs, lhs );
   strcpy ( xrhs, rhs );
 
   char *dot = ".";
-  char *l_token, *l_last, *r_last;
+  char *l_last, *r_last;
 
-  for ( l_token = strtok_r ( xlhs, dot, &l_last ) ; l_token ; l_token = strtok_r ( NULL, dot, &l_last ) ) {
-    printf ( "l_token: %s\n", l_token );
+  char *l_token = strtok_r ( xlhs, dot, &l_last );
+  char *r_token = strtok_r ( xrhs, dot, &r_last );
+
+  if ( l_token && !r_token ) {
+    return true;
   }
 
-  return true;
+  while ( l_token || r_token ) {
+    if ( l_token && r_token ) {
+      int l_numeric = isdigit ( l_token[0] );
+      int r_numeric = isdigit ( r_token[0] );
+
+      // printf ( "l_token: %s (N: %d) VS r_token: %s (N: %d)\n", l_token, l_numeric, r_token, r_numeric );
+
+      if ( l_numeric && r_numeric ) {
+        return atoi ( l_token ) < atoi ( r_token );
+      }
+      else if ( l_numeric ) {
+        return true;
+      }
+      else if ( r_numeric ) {
+        return false;
+      }
+      else {
+        int cmp = strcmp ( l_token, r_token );
+
+        if ( cmp ) {
+          return cmp;
+        }
+      }
+    }
+    else if ( l_token ) {
+      // printf ( "l_token: %s VS NULL\n", l_token );
+      return false;
+    }
+    else {
+      // printf ( "NULL VS r_token: %s\n", r_token );
+      return true;
+    }
+
+    l_token = strtok_r ( NULL, dot, &l_last );
+    r_token = strtok_r ( NULL, dot, &r_last );
+  }
+
+  return false;
 }
