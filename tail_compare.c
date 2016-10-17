b7eaf5405287da91f34ea800d204c25edd8854b6
Xavier Caron <xcaron@gmail.com>
2016-10-17 10:45:33 +0200

    cosmetics

--- tail_compare.c
+++ tail_compare.c
@@ -6,8 +6,7 @@
 // identifiers consisting of only digits are compared numerically and identifiers with letters or hyphens
 // are compared lexically in ASCII sort order. Numeric identifiers always have lower precedence than
 // non-numeric identifiers. A larger set of pre-release fields has a higher precedence than a smaller set,
-// if all of the preceding identifiers are equal.
-// Example:
+// if all of the preceding identifiers are equal. Example:
 // 1.0.0-alpha < 1.0.0-alpha.1 < 1.0.0-alpha.beta < 1.0.0-beta < 1.0.0-beta.2 < 1.0.0-beta.11 < 1.0.0-rc.1 < 1.0.0.
 
 bool tail_lt ( char *lhs, char *rhs ) {
