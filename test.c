26a422504f5097162ef8e21de26b66938718a72e
Xavier Caron <xcaron@gmail.com>
2016-10-17 10:17:49 +0200

    defined messages

--- test.c
+++ test.c
@@ -5,13 +5,13 @@
 int main () {
   plan ( 7 );
 
-  cmp_ok ( tail_lt ( "alpha",      "alpha.1"    ), "==", true );
-  cmp_ok ( tail_lt ( "alpha.1",    "alpha.beta" ), "==", true );
-  cmp_ok ( tail_lt ( "alpha.beta", "beta"       ), "==", true );
-  cmp_ok ( tail_lt ( "beta",       "beta.2"     ), "==", true );
-  cmp_ok ( tail_lt ( "beta.2",     "beta.11"    ), "==", true );
-  cmp_ok ( tail_lt ( "beta.11",    "rc.1"       ), "==", true );
-  cmp_ok ( tail_lt ( "rc.1",       ""           ), "==", true );
+  cmp_ok ( tail_lt ( "alpha",      "alpha.1"    ), "==", true, "alpha < alpha.1" );
+  cmp_ok ( tail_lt ( "alpha.1",    "alpha.beta" ), "==", true, "alpha.1 < alpha.beta" );
+  cmp_ok ( tail_lt ( "alpha.beta", "beta"       ), "==", true, "alpha.beta < beta" );
+  cmp_ok ( tail_lt ( "beta",       "beta.2"     ), "==", true, "beta < beta.2" );
+  cmp_ok ( tail_lt ( "beta.2",     "beta.11"    ), "==", true, "beta.2 < beta.11" );
+  cmp_ok ( tail_lt ( "beta.11",    "rc.1"       ), "==", true, "beta.11 < rc.1" );
+  cmp_ok ( tail_lt ( "rc.1",       ""           ), "==", true, "rc.1 < {empty}" );
 
   done_testing ();
 }
